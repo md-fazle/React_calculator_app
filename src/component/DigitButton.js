1e9bbdd13898de141cef5710d67d6b5ce9f232ce
md-fazle <rf.fazle.rabbi@gmail.com>
2024-02-10 20:54:51 +0600

    first commit

---
+++ src/component/DigitButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "./Welcome"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
